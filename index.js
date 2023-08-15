b19f086782e9ee0578dc8ec1a568bc386a906476
kreby455 <clebinhoalves2011@gmail.com>
2023-08-15 16:31:11 -0300

    request body

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 /*
     - Query params => meusite.com/user?name=cleber&age=21  //FILTRO
     - Route params => /users/2  //BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÍFICO
-
+    - Request Body => {"name":"Rodolfo", "age"}
 */
 
-app.get('/users/:id', (request, response) => {
-    const{ id } = request.params
-    return response.json({id})
+app.get('/users/', (request, response) => {
+    const {name, age } = request.body
+    return response.json({name, age})
 })
 
 app.listen(3001, 
